a75cba036d7448ad6e3f5795186c5d36ee368ef3
Joy Marie Navales <joy@bbo.com.ph>
2020-09-27 20:45:17 +0800

    podfilese

---
+++ pods/Target Support Files/Pods-SampleInterviewProject-iOS-SampleInterviewProject-iOSUITests/Pods-SampleInterviewProject-iOS-SampleInterviewProject-iOSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleInterviewProject_iOS_SampleInterviewProject_iOSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SampleInterviewProject_iOS_SampleInterviewProject_iOSUITests
+@end
---
+++ pods/Target Support Files/Pods-SampleInterviewProject-iOS-SampleInterviewProject-iOSUITests/Pods-SampleInterviewProject-iOS-SampleInterviewProject-iOSUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SampleInterviewProject_iOS_SampleInterviewProject_iOSUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SampleInterviewProject_iOS_SampleInterviewProject_iOSUITestsVersionString[];
+
